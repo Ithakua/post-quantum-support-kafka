4d4b73662e4c24d7e6e5471aaeb5d5dab36b6cf7
Ithakua <adnmauriz@gmail.com>
2024-05-27 12:06:27 +0200

    All testing groups added

--- testing/scripts/graphGenerator.py
+++ testing/scripts/graphGenerator.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Leer el archivo CSV
-df = pd.read_csv('../SecurityCustomProvider/handshake_times.csv')
+df = pd.read_csv('handshake_times.csv')
 
 # Mostrar la cabecera del DataFrame para verificar los datos
 print(df.head())
@@ -21,6 +21,30 @@ plt.ylabel('Time (miliseconds)')
 
 # Añadir una leyenda
 plt.legend()
+
+# Mostrar la gráfica
+plt.grid(True)
+plt.show()
+
+# Calcular la mediana de todos los valores para cada índice
+medians = df.median()
+
+# Excluir el primer elemento (el número de iteraciones)
+medians = medians.drop(df.columns[0])
+
+# Crear una nueva figura para el diagrama de barras
+plt.figure(figsize=(12, 6))
+
+# Crear un diagrama de barras con las medianas
+plt.bar(medians.index, medians.values)
+
+# Añadir título y etiquetas
+plt.title('Median Handshake Time')
+plt.xlabel('N Iteration')
+plt.ylabel('Time (miliseconds)')
+
+# Rotar las etiquetas del eje x para evitar que se solapen
+plt.xticks(rotation=45)
 
 # Mostrar la gráfica
 plt.grid(True)
